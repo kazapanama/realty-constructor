42c6920a1d3340df96a087200bed2239ddc841d6
Vova <kazap3r@gmail.com>
2022-06-01 23:22:53 +0300

    5 almoust working question pages

--- script.js
+++ script.js
@@ -4,7 +4,24 @@ const questionBlocks = document.querySelectorAll('.question-block');
 let qBlock1 = questionBlocks[0];
 let qBlock2 = questionBlocks[1];
 let qBlock3 = questionBlocks[2];
+let qBlock4 = questionBlocks[3];
+let qBlock5 = questionBlocks[4]
+
+//input of price
+let priceInput = document.querySelector('#price');
+qBlock4.children[0].value = 0;
+
+//getting checkboxes from last question page
+let detail_checks = document.querySelectorAll('[data-desc]')
+
+
+
+
+
+
+
 let obj ={};
+obj.options = [];
 let counter = 0;
 
 //next question block on click
@@ -46,6 +63,62 @@ qBlock3.children[6].addEventListener('click',question3);
 qBlock3.children[7].addEventListener('click',question3);
 
 
+
+qBlock4.children[1].addEventListener('click',()=> qBlock4.children[0].value = 0);
+
+// plus buttons
+qBlock4.children[2].addEventListener('click',plus)
+qBlock4.children[3].addEventListener('click',plus)
+qBlock4.children[5].addEventListener('click',plus)
+qBlock4.children[6].addEventListener('click',plus)
+
+
+
+
+
+//send buttons
+qBlock4.children[7].addEventListener('click',question4);
+qBlock4.children[8].addEventListener('click',skip);
+
+
+//checkboxes
+qBlock5.children[0].addEventListener('click',checkCheckbox);
+qBlock5.children[1].addEventListener('click',checkCheckbox);
+qBlock5.children[2].addEventListener('click',checkCheckbox);
+qBlock5.children[3].addEventListener('click',checkCheckbox);
+qBlock5.children[4].addEventListener('click',checkCheckbox);
+qBlock5.children[5].addEventListener('click',checkCheckbox);
+qBlock5.children[6].addEventListener('click',checkCheckbox);
+
+
+
+
+
+//last page submit
+qBlock5.children[7].addEventListener('click',question5);
+
+
+
+
+function checkCheckbox(e){
+    e.target.classList.toggle('checked');
+    if(e.target.classList.contains('checked')){
+        obj.options.push(e.target.dataset.desc);
+    }
+    else{
+        obj.options.splice(obj.options.indexOf(e.target.dataset.desc),1);
+    }
+}
+
+
+
+
+
+function plus(e){
+    qBlock4.children[0].value = parseInt(qBlock4.children[0].value) + parseInt(e.target.value);
+}
+
+
 function question1(e){
     obj.q1 = e.target.value;
 
@@ -60,12 +133,42 @@ function question2(e){
 
 function question3(e){
     obj.q3 = e.target.value;
+    
 
 
-    screen2.classList.add('hide')
+    nextQuestion();
 
-    finalOchka()
+    
 }
+
+
+function skip(){
+    obj.q4 = '.';
+    nextQuestion();
+}
+
+
+
+
+function question4(e){
+    obj.q4 = ` за ${qBlock4.children[0].value}.&#9166;`;
+
+
+    nextQuestion();
+
+    
+}
+
+function question5(e){
+    
+
+
+   finalOchka();
+
+    
+}
+
+
 
 
 function finalOchka(){
@@ -73,14 +176,10 @@ function finalOchka(){
     screen2.classList.add('hide');
     screen3.classList.remove('hide');
     let a = document.querySelector('.result-textarea');
-    a.innerHTML = `${obj.q1} ${obj.q2} в районі ${obj.q3}`;
+    a.innerHTML = `${obj.q1} ${obj.q2} в районі ${obj.q3}${obj.q4}
+    ${obj.options.join('. ')}`;
 }
 
-
-// let a = document.querySelector('.result-textarea')
-// a.innerHTML = `Здам в оренду двокімнатну квартиру в районі 12 школи за 5500 грн
-// Для сім’ї. На довгий термін. 
-// Квартира тепла та затишна, поруч є все необхідне для комфортного проживання, магазини, супермаркети.`
 
 
 
